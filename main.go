6eaeb83b3e51f2410e5e46eb08445e009718b590
叶俊青 <yejunqing@woda.ink>
2021-03-08 12:11:17 +0800

    调整日志中的时间格式

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	"flag"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
@@ -25,13 +24,7 @@ import (
 )
 
 func initLog() *zap.SugaredLogger {
-	hook := lumberjack.Logger{
-		Filename:   "./logs/package.log", // 日志文件路径
-		MaxSize:    10,                   // 每个日志文件保存的最大尺寸 单位：M
-		MaxBackups: 5,                    // 日志文件最多保存多少个备份
-		MaxAge:     7,                    // 文件最多保存多少天
-		Compress:   true,                 // 是否压缩, 压缩后1M约占20Kb
-	}
+
 
 	encoderConfig := zapcore.EncoderConfig{
 		TimeKey:        "time",
@@ -43,17 +36,29 @@ func initLog() *zap.SugaredLogger {
 		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
 		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
 		EncodeDuration: zapcore.SecondsDurationEncoder, //
+		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
 
 	// 设置日志级别
 	atomicLevel := zap.NewAtomicLevel()
 	atomicLevel.SetLevel(zap.InfoLevel)
-
-	_ = zapcore.NewCore(
+/*
+   hook := lumberjack.Logger{
+   		Filename:   "./logs/package.log", // 日志文件路径
+   		MaxSize:    10,                   // 每个日志文件保存的最大尺寸 单位：M
+   		MaxBackups: 5,                    // 日志文件最多保存多少个备份
+   		MaxAge:     7,                    // 文件最多保存多少天
+   		Compress:   true,                 // 是否压缩, 压缩后1M约占20Kb
+   	}
+	core := zapcore.NewCore(
 		zapcore.NewConsoleEncoder(encoderConfig),                                        // 编码器配置
 		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)), // 打印到控制台和文件
 		atomicLevel, // 日志级别
 	)
+	logger := zap.New(core)
+
+ */
+
 	config := zap.Config{
 		Level:       atomicLevel,
 		Development: false,
@@ -62,11 +67,13 @@ func initLog() *zap.SugaredLogger {
 			Thereafter: 100,
 		},
 		Encoding:         "console",
-		EncoderConfig:    zap.NewProductionEncoderConfig(),
+		EncoderConfig:    encoderConfig,
 		OutputPaths:      []string{"stderr"},
 		ErrorOutputPaths: []string{"stderr"},
 	}
 	logger,_ := config.Build()
+
+
 	sugar := logger.Sugar()
 
 	logger.Info("log 初始化成功")
